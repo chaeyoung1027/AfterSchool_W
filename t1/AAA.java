45782c7d2160b8b42036757c6d93005d9e2db4d3
chaeyoung1027 <83990991+chaeyoung1027@users.noreply.github.com>
2022-01-06 11:49:57 +0900

    public, private, protected, default 연습

---
+++ t1/AAA.java
@@ -0,0 +1,13 @@
+package Test.t1;
+
+//MainTest와 같은 패키지
+public class AAA {
+	public void accessTest() {
+		MainTest mt = new MainTest();
+//		같은 패키지일 때는 private만 빼고 모두 접근 가능
+		System.out.println(mt.n1);//public
+//		System.out.println(mt.n2);//private
+		System.out.println(mt.n3);;//protected
+		System.out.println(mt.n4);//default
+	}
+}

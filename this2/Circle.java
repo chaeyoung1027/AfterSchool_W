01211a97b041537aeab938a61f3d877e169dd283
chaeyoung1027 <83990991+chaeyoung1027@users.noreply.github.com>
2022-01-07 10:44:13 +0900

    원의 넓이, 둘레 구하기

---
+++ this2/Circle.java
@@ -0,0 +1,38 @@
+package Test.this2;
+
+public class Circle {
+	
+	private int r;
+	private double circum, area;
+
+	public double getCircum() {
+		return circum;
+	}
+
+	public double getArea() {
+		return area;
+	}
+	
+	public int getR() {
+		return r;
+	}
+	
+	public Circle(int r) {
+		this.r = r;
+	}
+	
+	public void calcCircum() {
+		circum = 2*Math.PI*r;
+	}
+	
+	public void calcArea() {
+		area = Math.PI * Math.pow(r, 2); //pow : Á¦°ö
+	}
+	
+	public void showInfo() {
+		calcCircum();
+		calcArea();
+		RunCircle.showPrint(this);
+	}
+
+}
